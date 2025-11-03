33c63a74653e99700686e90df73049723ee37c96
Oriana Mendez <ramiled0@gmail.com>
2025-11-03 18:26:20 -0400

    Add SVG illustration and update styles for main page layout
    - Added undraw_designer_efwz.svg as a hero image in the main page.
    - Refactored index.css to remove unused styles and maintain only essential variables.
    - Created styles.css for the main page with a gradient background and improved button styles.
    - Enhanced layout with flexbox for better alignment and responsiveness.

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,21 @@
+import "./styles.css";
+
 export const MainPage: React.FC = () => {
   return (
     <section>
-      <h1>Bienvenido</h1>
+      <img
+        src="src\assets\undraw_designer_efwz.svg"
+        alt="CV ilustración"
+        className="hero-image"
+      />
 
-      <button>Crear</button>
+      <div className="container">
+        <h1 className="titulo-principal">
+          Bienvenido a tu generador de currículum
+        </h1>
+        <h2 className="titulo-secundario">Empieza a crear tu CV en minutos</h2>
+        <button className="buttonMain">Crear</button>
+      </div>
     </section>
   );
 };
